67a5476ce24502554cd54d30bdcc65f93bc96510
Matthijs-V0s <matthijsvos283@gmail.com>
2025-09-15 15:16:35 +0200

    played with Timer

--- week2_timer/main.c
+++ week2_timer/main.c
@@ -1,41 +1,39 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-#include "driverlib.h"
+#include <msp430.h>
 
-//******************************************************************************
-//!
-//!   Empty Project that includes driverlib
-//!
-//******************************************************************************
-void main (void)
+int main(void)
 {
+WDTCTL = WDTPW | WDTHOLD;                   // Stop WDT
+
+// Configure GPIO
+P1DIR |= BIT0 | BIT1;                       // P1.0 and P1.1 output
+P1SEL0 |= BIT0 | BIT1;                      // P1.0 and P1.1 options
+//select
+
+PM5CTL0 &= ~LOCKLPM5;
+
+// Klok instellingen
+CSCTL0_H = CSKEY >> 8;                      // Unlock CS registers
+CSCTL1 = DCOFSEL_6;                         // Set DCO = 8MHz
+CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+CSCTL3 = DIVA__8 | DIVS__8 | DIVM__8;       // Set all dividers
+CSCTL0_H = 0;                               // Lock CS registers
+
+// Configure Timer0_A
+TA0CCR0 = 1000-1;                           // PWM Period CCR0
+TA0CCTL1 = OUTMOD_7;                        // CCR1 reset/set
+TA0CCR1 = 750;                              // CCR1 PWM duty cycle
+TA0CCTL2 = OUTMOD_7;                        // CCR2 reset/set
+TA0CCR2 = 250;                              // CCR2 PWM duty cycle
+TA0CTL = TASSEL__SMCLK | MC__UP | TACLR;    // SMCLK, up mode, clear TAR
+while (1) {
+    TA0CCR1 += 10;
+    __delay_cycles(100000);
+    if (TA0CCR1 == 1000) {
+        TA0CCR1 = 0;
+    }
+}
+
+// Sleep and do nothing
+__bis_SR_register(LPM0_bits);          // Enter LPM0
+__no_operation();                           // For debugger
 }
