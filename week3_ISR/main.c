1741b4134c8472027d65fcac61afb610d4af6c71
Matthijs-V0s <matthijsvos283@gmail.com>
2025-09-16 14:42:51 +0200

    opdracht ISR ledje

--- week3_ISR/main.c
+++ week3_ISR/main.c
@@ -29,13 +29,48 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * --/COPYRIGHT--*/
-#include "driverlib.h"
+#include <msp430.h>
 
-//******************************************************************************
-//!
-//!   Empty Project that includes driverlib
-//!
-//******************************************************************************
 void main (void)
 {
+    WDTCTL = WDTPW | WDTHOLD;           //stop WDT
+
+    //P1.1
+    P1DIR &= ~BIT1;                     //set P1.1 as input
+    P1REN |= BIT1;                      //eneble pull-resistor
+    P1OUT |= BIT1;                      //set pull-up resistor
+    P1IE |= BIT1;                       //enable interrupt
+    P1IES |= BIT1;                      //set detect falling edge
+    P1IFG &= ~BIT1;                     //clear interrupt flag
+
+    //P1.0
+    P1DIR |= BIT0;                      //set P1.0 as output
+    P1OUT |= BIT0;
+
+    PM5CTL0 &= ~LOCKLPM5;               //enable GPIO pins
+
+    __bis_SR_register(GIE);       //enable global interrupt
+
+    
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void pushbutton_ISR (void) {
+    switch (__even_in_range(P1IV, 0x10)) {
+        case 0x00: break;       //none
+        case 0x02: break;       //Pin 0
+        case 0x04:              //Pin 1
+        P1OUT ^= BIT0;                  //toggle P1.0 (LED)
+        P1IFG &= ~BIT1;
+        break;       
+        case 0x06: break;       //Pin 2
+        case 0x08: break;       //Pin 3
+        case 0x0A: break;       //Pin 4
+        case 0x0C: break;       //Pin 5
+        case 0x0E: break;       //Pin 6
+        case 0x10: break;       //Pin 7
+        default: _never_executed();
+    }
+
+    
 }
