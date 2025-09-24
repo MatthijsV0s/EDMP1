c560940058590aa35a52b01ffb8603a6bff7cef3
Matthijs-V0s <matthijsvos283@gmail.com>
2025-09-24 12:59:36 +0200

    add function to toggle pin

--- week4/include/gpio_lib.h
+++ week4/include/gpio_lib.h
@@ -1,6 +1,7 @@
 #ifndef GPIO_LIB_H
 #define GPIO_LIB_H
 
-void pinSet(uint16_t port, uint16_t bit, bool val);
+void pinSet(uint16_t port, uint16_t bit, bool val);         // Set port.bit to val
+void pinToggle(uint16_t port, uint16_t bit);                // Toggle port.bit
 
 #endif /* GPIO_LIB_H */
