353a0194dc8d5e3400dda37911f2e17df6e88dc3
Matthijs-V0s <matthijsvos283@gmail.com>
2025-09-24 15:20:32 +0200

    add documantation with Doxygen

---
+++ week4/docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gpio_5flib_2eh_0',['gpio_lib.h',['../gpio__lib_8h.html',1,'']]]
+];
--- week4/include/gpio_lib.h
+++ week4/include/gpio_lib.h
@@ -1,3 +1,11 @@
+/**
+ * @file
+ * gpio_lib.h
+ *
+ * Created on: 23-09-2025
+ *     Author: M. Vos
+ */
+
 #ifndef GPIO_LIB_H
 #define GPIO_LIB_H
 
@@ -13,11 +21,66 @@ typedef enum {
     tertiary
 } purposeFunction;
 
-void pinSet(uint16_t port, uint16_t bit, bool val);         // Set port.bit to val
-void pinToggle(uint16_t port, uint16_t bit);                // Toggle port.bit
-bool pinGet(uint16_t port, uint16_t bit);                   // Get digital value of port.bit
-void pinConfigInput(uint16_t port, uint16_t bit, bool pullResistor, bool pullUP, bool IES, bool IE);    // Set pin configuration for port.bit
-void pinSetDir(uint16_t port, uint16_t bit, bool val);      // Set direction of port.bit to val
+/**
+ *
+ * @brief Sets an output to a pin.
+ * @param[in] port: The port register address.
+ * @param[in] pin:  The specific pin to toggle on that port.
+ * @param[in] val:  The value that the pin gets.
+ *
+ */
+void pinSet(uint16_t port, uint16_t bit, bool val);
+
+/**
+ *
+ * @brief Toggles a pin.
+ * @param[in] port: The port register address.
+ * @param[in] pin:  The specific pin to toggle on that port.
+ *
+ */
+void pinToggle(uint16_t port, uint16_t bit);
+
+/**
+ *
+ * @brief Gets an value of a pin.
+ * @param[in] port: The port register address.
+ * @param[in] pin:  The specific pin to toggle on that port.
+ * @returns The value of the pin.
+ *
+ */
+bool pinGet(uint16_t port, uint16_t bit);
+
+/**
+ * Sets all the input configurations of a pin except the direction and function of the pin.
+ * @brief Sets the input configuration of a pin.
+ * @param[in] port:         The port register address.
+ * @param[in] pin:          The specific pin to toggle on that port.
+ * @param[in] pullResistor: Indicates if the pin gets a pullresistor.
+ * @param[in] pullUP:       Indicates which resistor the pin get: 1 Up/0 Down.
+ * @param[in] IES:          Indicates on which edge an interrupt is triggerd.
+ * @param[in] IE:           Indicates if interrupt is enabled.
+ *
+ */
+void pinConfigInput(uint16_t port, uint16_t bit, bool pullResistor, bool pullUP, bool IES, bool IE);
+
+/**
+ *
+ * @brief Sets the direction of a pin.
+ * @param[in] port: The port register address.
+ * @param[in] pin:  The specific pin to toggle on that port.
+ * @param[in] val:  The direction of the pin: 1 OUTPUT/0 INPUT.
+ *
+ */
+void pinSetDir(uint16_t port, uint16_t bit, bool val);
+
+/**
+ * Sets the function of a pin to Digital/Primary/Secondary/Teriary.
+ * @brief Sets the function of a pin.
+ * @param[in] port: The port register address.
+ * @param[in] pin:  The specific pin to toggle on that port.
+ * @param[in] pf:   The function of the pin.
+ *
+ */
 void pinConfigFunction(uint16_t port, uint16_t bit, purposeFunction pf);
 
 #endif /* GPIO_LIB_H */
