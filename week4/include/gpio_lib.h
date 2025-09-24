b765a86eb59a005fe00ee8e9a85bb25a00caecfd
Matthijs-V0s <matthijsvos283@gmail.com>
2025-09-24 13:42:15 +0200

    add function to set input pin configuration

--- week4/include/gpio_lib.h
+++ week4/include/gpio_lib.h
@@ -1,13 +1,14 @@
 #ifndef GPIO_LIB_H
 #define GPIO_LIB_H
 
-#define PORT1 0x0000;
-#define PORT2 0x0001;
-#define PORT3 0x0020;
-#define PORT4 0x0021;
+#define PORT1 0x0000
+#define PORT2 0x0001
+#define PORT3 0x0020
+#define PORT4 0x0021
 
 void pinSet(uint16_t port, uint16_t bit, bool val);         // Set port.bit to val
 void pinToggle(uint16_t port, uint16_t bit);                // Toggle port.bit
 bool pinGet(uint16_t port, uint16_t bit);                   // Get digital value of port.bit
+void pinConfigInput(uint16_t port, uint16_t bit, bool pullResistor, bool pullUP, bool IES, bool IE);    // Set pin configuration for port.bit
 
 #endif /* GPIO_LIB_H */
--- week4/src/gpio_lib.c
+++ week4/src/gpio_lib.c
@@ -11,3 +11,10 @@ void pinToggle(uint16_t port, uint16_t bit){
 bool pinGet(uint16_t port, uint16_t bit){
     return (*(&P1IN+port)&bit);
 }
+
+void pinConfigInput(uint16_t port, uint16_t bit, bool pullResistor, bool pullUP, bool IES, bool IE){
+    pullResistor ? (*(&P1REN+port) |= bit) : (*(&P1REN+port) &= ~bit);
+    pullUp ? (*(&P1OUT+port) |= bit) : (*(&P1OUT+port) &= ~bit);
+    IES ? (*(&P1IES+port) |= bit) : (*(&P1IES+port) &= ~bit);
+    IE ? (*(&P1IE+port) |= bit) : (*(&P1IE+port) &= ~bit);
+}
